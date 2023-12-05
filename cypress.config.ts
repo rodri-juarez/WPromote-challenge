268bb2e02df2c391bb6121052e5a0412bb38775f
Rodrigo Juarez <juarezr100@gmail.com>
2023-12-05 10:42:55 -0300

    test: update e2e

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
+    setupNodeEvents() {
     },
-    baseUrl: 'http://localhost:3001',
+    baseUrl: 'http://localhost:3000',
   },
 });
--- cypress/e2e/product.cy.ts
+++ cypress/e2e/product.cy.ts
@@ -3,9 +3,7 @@ describe('Product Detail Page Test', () => {
   it('Should navigate to product detail page from home', () => {
     cy.visit('/');
 
-    cy.contains('See more').click();
-
-    cy.url().should('include', '/products');
+    cy.contains('View product').click();
 
     cy.contains('Shop Now').should('be.visible');
 
