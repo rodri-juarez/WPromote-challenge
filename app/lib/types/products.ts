ea907a7ca29ba4ca3e80fb250b13dae43c0076a3
Rodrigo Juarez <juarezr100@gmail.com>
2023-12-04 23:15:38 -0300

    perf(lib): add Product types

---
+++ app/lib/types/products.ts
@@ -0,0 +1,45 @@
+export interface ProductsList {
+  products: Products;
+}
+
+export interface Products {
+  edges: ProductsEdge[];
+}
+
+export interface ProductsEdge {
+  node: Product;
+}
+
+export interface Product {
+  id: string;
+  title: string;
+  description: string;
+  featuredImage: FeaturedImage;
+  variants: Variants;
+}
+
+export interface FeaturedImage {
+  id: string;
+  url: string;
+}
+
+export interface Variants {
+  edges: VariantsEdge[];
+}
+
+export interface VariantsEdge {
+  node: FluffyNode;
+}
+
+export interface FluffyNode {
+  price: Price;
+}
+
+export interface Price {
+  amount: string;
+  currencyCode: CurrencyCode;
+}
+
+export enum CurrencyCode {
+  CAD = "CAD",
+}
